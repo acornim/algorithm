b346d4f1cbd30853022c0287c7b4e1f003f07207
acornim <youn_306@naver.com>
2020-01-09 14:47:48 +0900

    2753

---
+++ Problem/2753/2753.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int year;
+    cin >> year;
+    while(1){
+        if(year >1 && year <4000){
+            break;
+        }else{
+            continue;
+        }
+    }
+    if(year){
+        if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            cout << 1;
+        else
+        {
+            cout << 0;
+        }
+        
+    }
+    return 0;
+}
