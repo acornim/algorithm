f8bee93686a5ced7b2c9188dd25536c080cabfa6
acornim <youn_306@naver.com>
2020-01-20 10:40:07 +0900

    11729

---
+++ Problem/11729/11729.cpp
@@ -0,0 +1,24 @@
+// Mon Jan 20 2020 하노이 탑 쌓기 
+#include <iostream>
+
+using namespace std;
+void hanoi(int n, int start, int mid, int end){
+    if(n==1)
+        cout << start << " " << end << '\n';
+    else{
+        hanoi(n - 1, start, end, mid); //n-1 원판들 -> 2번기둥
+        cout << start << " " << end << '\n'; //1번 기둥의 n번째 원판 -> 3번 기둥으로
+        hanoi(n - 1, mid, start, end);
+
+    }
+        
+}
+int main(){
+    int n; //원판 갯수
+    cin >> n;
+
+    cout << (1 << n) - 1 << '\n';  // (1 << n)은 shift operation! 
+
+    hanoi(n,1,2,3);
+    return 0;
+}
